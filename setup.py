cb1fc68eda84aa683479619787d11d25a9579a21
JulienML <julien-devos@live.fr>
2025-04-10 20:15:28 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ import os
 import patoolib
 
 # Create the test directory if it doesn't exist
-if not os.path.exists("./test"):
-    os.makedirs("./test")
+if not os.path.exists("./datasets"):
+    os.makedirs("./datasets")
 
 # DIDA dataset download link
 url = f"https://drive.google.com/uc?id=1d-U-lxIoS5QuPEYPvHA2-Bm4pULsTb06"
 
 # Download the file
-gdown.download(url, "./test/DIDA.rar")
+gdown.download(url, "./datasets/DIDA.rar")
 
 # Uncompress the downloaded file
-patoolib.extract_archive("./test/DIDA.rar", outdir="./test/")
+patoolib.extract_archive("./datasets/DIDA.rar", outdir="./datasets/")
 
 # Rename the extracted folder
-os.rename("./test/10000", "./test/DIDA")
+os.rename("./datasets/10000", "./datasets/DIDA")
 
 # Remove the rar file after extraction
-os.remove("./test/DIDA.rar")
+os.remove("./datasets/DIDA.rar")
